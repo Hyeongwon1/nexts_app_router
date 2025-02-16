154087a76b3904e3365eea137d4d67a97990b92d
hwseo <hyeongwon1@naver.com>
2025-02-16 21:51:06 +0900

    앱라우터 ,서버컴포넌트,네비게이터

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -1,5 +1,5 @@
 import {ReactNode} from "react";
-import Searchbar from "@/app/(with-searchbar)/searchbar";
+import Searchbar from "@/componets/searchbar";
 
 export default function Layout({children}: { children: ReactNode; }) {
   return <div>
--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,14 +1,15 @@
 import styles from "./page.module.css";
-import ClientComponent from "@/app/(with-searchbar)/client-component";
-import ServerComponent from "@/app/(with-searchbar)/server-component";
+import ClientComponent from "@/componets/client-component";
+import ServerComponent from "@/componets/server-component";
 
 export default function Home() {
 
   return (
       <div className={styles.page}>인덱스 페이지
-      <ClientComponent>
-        <ServerComponent></ServerComponent>
-      </ClientComponent>
+        <ClientComponent>
+          <ServerComponent></ServerComponent>
+        </ClientComponent>
       </div>
   );
 }
+``
