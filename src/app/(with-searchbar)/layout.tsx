3f03823e7a0f5d6e480288d9446f53bae2ee8c01
hwseo <hyeongwon1@naver.com>
2025-02-16 22:47:18 +0900

    UI적용

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -1,9 +1,11 @@
-import {ReactNode} from "react";
-import Searchbar from "@/componets/searchbar";
+import { ReactNode, Suspense } from "react";
+import Searchbar from "../../components/searchbar";
 
-export default function Layout({children}: { children: ReactNode; }) {
-  return <div>
-    <Searchbar/>
-    {children}
-  </div>
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <Searchbar />
+      {children}
+    </div>
+  );
 }
